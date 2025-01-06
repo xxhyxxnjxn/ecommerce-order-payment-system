6f75b440c418c2d5d44a9b5a75487b1fb9330628
xxhyxxnjxn <wjdguswls55@gmail.com>
2025-01-06 21:26:19 +0900

    first commit
    (cherry picked from commit 33da1716c89b4ea9e0103798d9134b1e0f94157d)

---
+++ src/test/java/imgsystem/ecommerceorderpaymentsystem/EcommerceOrderPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package imgsystem.ecommerceorderpaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderPaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

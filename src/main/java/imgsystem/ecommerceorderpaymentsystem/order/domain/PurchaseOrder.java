6a336fbaa6db927e35e94f0d1406da125b74dab7
xxhyxxnjxn <wjdguswls55@gmail.com>
2025-01-08 22:44:46 +0900

    ERD를 바탕으로 PurchageOrder Entity 작성

--- src/main/java/imgsystem/ecommerceorderpaymentsystem/order/domain/PurchaseOrder.java
+++ src/main/java/imgsystem/ecommerceorderpaymentsystem/order/domain/PurchaseOrder.java
@@ -1,13 +1,41 @@
 package imgsystem.ecommerceorderpaymentsystem.order.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name="purchase_order")
+@Getter
+@Setter
 public class PurchaseOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_id", columnDefinition = "BINARY(16)", nullable = false, updatable = false)
+    private UUID orderId;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(name="phone_number",nullable = false)
     private String phoneNumber;
+
+    @Column(name = "order_state",nullable = false)
+    private String orderState;
+
+    @Column(name="payment_id")
+    private String paymentId;
+
+    @Column(name = "total_price", nullable = false)
+    private int totalPrice;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt = LocalDateTime.now();
 }
